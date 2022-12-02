1388a0dad9a9f2ada2554f5d561f60cd37869fd0
Rarity110 <rarity110@mail.ru>
2022-12-02 23:57:48 +0300

    fix: fix modal width

--- src/styledComponents/CustomizedInput.ts
+++ src/styledComponents/CustomizedInput.ts
@@ -4,7 +4,6 @@ const CustomizedInput = styled.input`
   color: #223059;
   width: 100%;
   font-size: 18px;
-  /* font-weight: 500; */
   height: '80%';
   background-color: transparent;
   border: none;
